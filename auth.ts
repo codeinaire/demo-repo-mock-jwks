8951671e845033d71747d957146e4fb27b76746e
Levin Keller <post@levinkeller.de>
2019-11-07 14:03:50 +0100

    Refactor and clean up code. Add linting.

--- auth.ts
+++ auth.ts
@@ -1,127 +1,144 @@
-require('dotenv').config({ silent: true });
+import jwksClient from 'jwks-rsa'
+import jwt from 'jsonwebtoken'
+import util from 'util'
 
-import jwksClient from 'jwks-rsa';
-import jwt from 'jsonwebtoken';
-import util from 'util';
+import { APIGatewayProxyEvent } from 'aws-lambda'
 
-import { APIGatewayProxyEvent } from 'aws-lambda';
+export const createCheckScopesAndResolve = ({
+  jwksUri,
+  audience,
+  issuer,
+}: {
+  jwksUri: string
+  audience: string
+  issuer: string
+}) => {
+  const client = jwksClient({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 10,
+    jwksUri,
+  })
 
-export default class Auth implements IAuth {
-  constructor() {}
+  const retrieveSigningKey = util.promisify(client.getSigningKey)
 
-  private async getSigningKey(keyId: string): Promise<string> {
-    console.log('testing', keyId);
-    const client = jwksClient({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 10,
-      jwksUri: process.env.JWKS_URI || ''
-    });
-
-    console.log('testing####', client);
-
-    const retrieveSigningKey = util.promisify(client.getSigningKey);
-    console.log('retrieveSigningKey####', retrieveSigningKey);
-    const retrievedKey = await retrieveSigningKey(keyId);
-    console.log('retrievedKey####', retrievedKey);
-    return (retrievedKey as jwksClient.CertSigningKey).publicKey ||
-    (retrievedKey as jwksClient.RsaSigningKey).rsaPublicKey;
+  const getSigningKey = async (keyId: string): Promise<string> => {
+    const retrievedKey = await retrieveSigningKey(keyId)
+    return (
+      (retrievedKey as jwksClient.CertSigningKey).publicKey ||
+      (retrievedKey as jwksClient.RsaSigningKey).rsaPublicKey
+    )
   }
 
-  private extractBearerToken(event: APIGatewayProxyEvent): string {
-    const tokenString = event.headers.authorization;
+  const extractBearerToken = (event: APIGatewayProxyEvent): string => {
+    const tokenString = event.headers.authorization
     if (!tokenString) {
-        throw new Error('Error: Expected "event.headers.authorization" parameter to be set');
+      throw new Error(
+        'Expected "event.headers.authorization" parameter to be set',
+      )
     }
 
-    const match = tokenString.match(/^Bearer (.*)$/);
+    const match = tokenString.match(/^Bearer (.*)$/)
     if (!match || match.length < 2) {
-        throw new Error(`Error: Invalid Authorization token - ${tokenString} does not match "Bearer .*"`);
+      throw new Error(
+        `Invalid Authorization token - ${tokenString} does not match "Bearer .*"`,
+      )
     }
 
-    return match[1];
+    return match[1]
   }
 
-  private async verifyToken(event: APIGatewayProxyEvent): Promise<IScopeAndId> {
-    const token = this.extractBearerToken(event);
+  const verifyToken = async (
+    event: APIGatewayProxyEvent,
+  ): Promise<IScopeAndId> => {
+    const token = extractBearerToken(event)
 
-    const decoded: IDecodedToken = jwt.decode(token, { complete: true }) as IDecodedToken;
+    const decoded: IDecodedToken = jwt.decode(token, {
+      complete: true,
+    }) as IDecodedToken
     if (!decoded || !decoded.header || !decoded.header.kid) {
-      throw new Error('Error: Invalid Token');
+      throw new Error('Invalid Token')
     }
 
-    const rsaOrCertSigningKey: string = await this.getSigningKey(decoded.header.kid);
+    const rsaOrCertSigningKey: string = await getSigningKey(decoded.header.kid)
 
     const jwtOptions = {
-      audience: process.env.AUDIENCE,
-      issuer: process.env.TOKEN_ISSUER
+      audience,
+      issuer,
     }
-    const verifiedToken: IVerifiedToken = await jwt.verify(token, rsaOrCertSigningKey, jwtOptions) as IVerifiedToken;
+    const verifiedToken: IVerifiedToken = (await jwt.verify(
+      token,
+      rsaOrCertSigningKey,
+      jwtOptions,
+    )) as IVerifiedToken
 
-    const scopes: Array<string> = verifiedToken.scope.split(' ');
+    const scopes: Array<string> = verifiedToken.scope.split(' ')
 
     return {
       principleId: verifiedToken.sub,
-      scopes: scopes
+      scopes: scopes,
     }
   }
 
-  public async checkScopesAndResolve(event: APIGatewayProxyEvent, expectedScopes: [string]): Promise<string> {
-    try {
-      const verifiedToken = await this.verifyToken(event);
+  return async (
+    event: APIGatewayProxyEvent,
+    expectedScopes: [string],
+  ): Promise<string> => {
+    const verifiedToken = await verifyToken(event)
 
-      const scopes = verifiedToken.scopes;
+    const scopes = verifiedToken.scopes
 
-      if (!scopes) {
-        throw new Error('Error: No scopes supplied!');
-      }
+    if (!scopes) {
+      throw new Error('No scopes supplied!')
+    }
 
-      const scopesMatch = expectedScopes.some(scope => scopes.indexOf(scope) !== -1);
-      if(scopesMatch) {
-        return verifiedToken.principleId;
-      } else {
-        throw new Error('Error: You are not authorized!');
-      }
-    } catch (error) {
-      return error;
+    const scopesMatch = expectedScopes.some(
+      scope => scopes.indexOf(scope) !== -1,
+    )
+    if (scopesMatch) {
+      return verifiedToken.principleId
+    } else {
+      throw new Error('You are not authorized!')
     }
   }
 }
 
-
 export interface IVerifiedToken {
-  iss: string;
-  sub: string;
-  aud: [string];
-  iat: number;
-  exp: number,
-  azp: string;
-  scope: string;
+  iss: string
+  sub: string
+  aud: [string]
+  iat: number
+  exp: number
+  azp: string
+  scope: string
 }
 
 export interface IDecodedToken {
   header: {
-    typ: string;
-    alg: string;
-    kid: string;
-  },
+    typ: string
+    alg: string
+    kid: string
+  }
   payload: {
-    iss: string;
-    sub: string;
-    aud: [string];
-    iat: number;
-    exp: number;
-    azp: string;
-    scope: string;
-  },
-  signature: string;
+    iss: string
+    sub: string
+    aud: [string]
+    iat: number
+    exp: number
+    azp: string
+    scope: string
+  }
+  signature: string
 }
 
 export interface IScopeAndId {
-  principleId: string;
-  scopes: Array<string>;
+  principleId: string
+  scopes: Array<string>
 }
 
 export interface IAuth {
-  checkScopesAndResolve: (arg0: APIGatewayProxyEvent, arg1: [string]) => Promise<string>;
+  checkScopesAndResolve: (
+    arg0: APIGatewayProxyEvent,
+    arg1: [string],
+  ) => Promise<string>
 }
